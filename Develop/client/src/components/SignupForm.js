95d720b21266ab83b6c83940098f58abefb4f848
Danny <dl667@cornell.edu>
2022-12-07 21:41:05 -0500

    deleted unused imports (from process of converting RESTful -> graphql)

--- Develop/client/src/components/SignupForm.js
+++ Develop/client/src/components/SignupForm.js
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { createUser } from '../utils/API';
 import { ADD_USER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 import Auth from '../utils/auth';
