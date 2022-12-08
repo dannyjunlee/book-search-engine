cae8a5f71eb6ff53c3c2b6bd536c73b5243b5621
Danny <dl667@cornell.edu>
2022-12-07 20:44:39 -0500

    resolvers and typeDefs

--- Develop/server/schemas/resolvers.js
+++ Develop/server/schemas/resolvers.js
@@ -4,14 +4,16 @@ const { signToken } = require('../utils/auth');
 
 const resolvers = {
     Query: {
-        getSingleUser: async (parent, args, context) => {
-            const params = args.username ? args.username : args._id;
-            return User.findOne({ params }).populate('savedBooks')
+        me: async (parent, args, context) => {
+            if (context.user) {
+                return User.findOne({ _id: context.user._id }).populate('savedBooks');
+            }
+            throw new AuthenticationError('You need to be logged in');
         }
     },
 
     Mutation: {
-        createUser: async (parent, { username, email, password }) => {
+        addUser: async (parent, { username, email, password }) => {
             const user = await User.create({ username, email, password });
             const token = signToken(user);
             return { token, user };
@@ -48,7 +50,7 @@ const resolvers = {
             throw new AuthenticationError('You need to be logged in!');
         },
 
-        deleteBook: async (parent, { bookId }, context) => {
+        removeBook: async (parent, { bookId }, context) => {
             if (context.user) {
                 const user = await User.findOneAndUpdate(
                     { _id: context.user._id },
--- Develop/server/schemas/typeDefs.js
+++ Develop/server/schemas/typeDefs.js
@@ -3,41 +3,40 @@ const { gql } = require('apollo-server-express');
 const typeDefs = gql`
     type Book {
         _id: ID
-        authors: [String]
-        # authors: String
-        description: String
         bookId: String
+        authors: [String]
+        description: String
+        title: String
         image: String
         link: String
-        title: String
     }
 
     type User {
         _id: ID
         username: String
         email: String
-        password: String
+        bookCount: Int
         savedBooks: [Book]
     }
 
     input savedBook {
         authors: [String]
         description: String
+        title: String
         bookId: String
         image: String
         link: String
-        title: String
     }
 
     type Query {
-        getSingleUser(_id: String, username: String): User
+        me: User
     }
 
     type Mutation {
-        createUser(username: String, email: String, password: String): Auth
         login(email: String!, password: String!): Auth
+        addUser(username: String, email: String, password: String): Auth
         saveBook(input: savedBook!): User
-        deleteBook(bookId: String): User
+        removeBook(bookId: String): User
     }
 
     type Auth {
